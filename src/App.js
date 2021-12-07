afce68d48b37293836b46084c5d185341c629e69
erysimum <cheirierysimum@gmail.com>
2021-12-07 15:18:08 +1100

    signin component created

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 import HatsPage from './pages/hatspage/hatspage.component.jsx';
 import { Route, Switch } from 'react-router-dom';
 import Shop from './././pages/shop/shop.component';
+import Header from './components/Header/header.component.jsx';
+import SignInSignUp from './pages/signin-signup/signin-signup.component';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={Shop} />
+        <Route exact path='/signin' component={SignInSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,9 @@
+import SignIn from '../../components/signin-signup/signin.component';
+
+const SignInSignUp = () => (
+  <div>
+    <SignIn />
+  </div>
+);
+
+export default SignInSignUp;
